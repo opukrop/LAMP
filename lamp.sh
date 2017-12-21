5fddda9f51814ac9b1f5d520ef33f07b3d2a127b
torlov <t1moxaorlov@gmail.com>
2017-12-21 15:30:10 +0200

    mod2 OS checking on script

--- lamp.sh
+++ lamp.sh
@@ -16,23 +16,21 @@ Yellow='\033[0;33m'       # Yellow
 Purple='\033[0;35m'       # Purple
 Cyan='\033[0;36m'         # Cyan
 
-# Checking if user root
+################ Checking if user root ##########################
 if [ "$EUID" -ne 0 ]
   then
   	echo -e "$Red You must run this as root user.. $Color_Off"
   exit
 fi
 
-# Check OS
+########################## Checking OS #############################
 
 if [ -f /etc/os-release ]; then
-    # freedesktop.org and systemd
     . /etc/os-release
     OS=$NAME
     VER=$VERSION_ID
 
 else
-    # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
     OS=$(uname -s)
     VER=$(uname -r)
 fi
